24486362aab5c1807f655dbb0c619534bae53dd2
GiantQuartz <giantquartz@gmail.com>
2020-03-14 13:13:18 +0100

    ProviderAsyncTask: Create methods for curl management

--- src/GiantQuartz/provider/ProviderAsyncTask.php
+++ src/GiantQuartz/provider/ProviderAsyncTask.php
@@ -4,6 +4,7 @@
 namespace GiantQuartz\provider;
 
 
+use Exception;
 use pocketmine\scheduler\AsyncTask;
 
 abstract class ProviderAsyncTask extends AsyncTask {
@@ -12,14 +13,14 @@ abstract class ProviderAsyncTask extends AsyncTask {
     protected $secretKey;
 
     /** @var string */
-    private const QUEUE_URL = "https://plugin.tebex.io/queue";
+    protected const QUEUE_URL = "https://plugin.tebex.io/queue/";
 
     public function __construct(Provider $provider) {
         $this->secretKey = $provider->getSecretKey();
     }
 
-    protected function getCurlSession() {
-        $session = curl_init(self::QUEUE_URL);
+    protected function getCurlSession(string $url = self::QUEUE_URL) {
+        $session = curl_init($url);
         curl_setopt($session, CURLOPT_HTTPHEADER, [
             "X-Tebex-Secret: " . $this->secretKey
         ]);
@@ -27,6 +28,31 @@ abstract class ProviderAsyncTask extends AsyncTask {
         curl_setopt($session, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($session, CURLOPT_TIMEOUT, 5);
         return $session;
+    }
+
+    protected function setCurlOptions($curl, array $options): void {
+        foreach($options as $option => $value) {
+            curl_setopt($curl, $option, $value);
+        }
+    }
+
+    /**
+     * @param $curl
+     * @throws Exception
+     */
+    protected function executeCurl($curl): void {
+        $this->checkCurlResult(curl_exec($curl), curl_error($curl));
+    }
+
+    /**
+     * @param $result
+     * @param $error
+     * @throws Exception
+     */
+    private function checkCurlResult($result, $error): void {
+        if($result === false) {
+            throw new Exception("Query error: $error");
+        }
     }
 
 }
