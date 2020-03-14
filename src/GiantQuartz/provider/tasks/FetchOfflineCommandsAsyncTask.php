a55b91008358574d40e08f0209365ce81ad11bb2
GiantQuartz <giantquartz@gmail.com>
2020-03-14 15:33:37 +0100

    Complete FetchOfflineCommandsAsyncTask

--- src/GiantQuartz/provider/tasks/FetchOfflineCommandsAsyncTask.php
+++ src/GiantQuartz/provider/tasks/FetchOfflineCommandsAsyncTask.php
@@ -4,12 +4,32 @@
 namespace GiantQuartz\provider\tasks;
 
 
+use Exception;
 use GiantQuartz\provider\ProviderAsyncTask;
+use pocketmine\command\ConsoleCommandSender;
 
 class FetchOfflineCommandsAsyncTask extends ProviderAsyncTask {
 
+    /**
+     * @throws Exception
+     */
     public function onRun(): void {
-        // TODO: Implement onRun() method.
+        $this->executeCurl($this->getCurlSession(self::QUEUE_URL . "offline-commands"));
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function onCompletion(): void {
+        $plugin = $this->getBuycraft();
+
+        $commandIdsExecuted = [];
+        foreach($this->getResult()["commands"] as $commandData) {
+            $commandIdsExecuted[] = $commandData["id"];
+            $plugin->getServer()->getCommandMap()->dispatch(new ConsoleCommandSender(), str_replace("{name}", $commandData["player"]["name"], $commandData["command"]));
+        }
+
+        $plugin->getProvider()->removeCommands($commandIdsExecuted);
     }
 
 }
